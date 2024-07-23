ca95bc4de68abcf828f48827cd0d568d64fc669e
Shrey Anish G <shreyanish.g@gmail.com>
2024-07-24 04:21:40 +0530

    misc

--- src/components/Marquee/index.tsx
+++ src/components/Marquee/index.tsx
@@ -1,47 +1,47 @@
 import { cn } from "@/lib/utils";
 import Marquee from "@/components/magicui/marquee";
 
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
+// const reviews = [
+//   {
+//     name: "Jack",
+//     username: "@jack",
+//     body: "I've never seen anything like this before. It's amazing. I love it.",
+//     img: "https://avatar.vercel.sh/jack",
+//   },
+//   {
+//     name: "Jill",
+//     username: "@jill",
+//     body: "I don't know what to say. I'm speechless. This is amazing.",
+//     img: "https://avatar.vercel.sh/jill",
+//   },
+//   {
+//     name: "John",
+//     username: "@john",
+//     body: "I'm at a loss for words. This is amazing. I love it.",
+//     img: "https://avatar.vercel.sh/john",
+//   },
+//   {
+//     name: "Jane",
+//     username: "@jane",
+//     body: "I'm at a loss for words. This is amazing. I love it.",
+//     img: "https://avatar.vercel.sh/jane",
+//   },
+//   {
+//     name: "Jenny",
+//     username: "@jenny",
+//     body: "I'm at a loss for words. This is amazing. I love it.",
+//     img: "https://avatar.vercel.sh/jenny",
+//   },
+//   {
+//     name: "James",
+//     username: "@james",
+//     body: "I'm at a loss for words. This is amazing. I love it.",
+//     img: "https://avatar.vercel.sh/james",
+//   },
+// ];
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
+// const firstRow = reviews.slice(0, reviews.length / 2);
+// const secondRow = reviews.slice(reviews.length / 2);
 
 // Skipped existing imports and code for brevity
 
@@ -75,7 +75,7 @@ const ReviewCard = ({
             <p className="text-xs font-medium text-black/40">{designation}</p>
           </div>
         </div>
-        <blockquote className="mt-2 text-sm text-gray-200">{company}</blockquote>
+        <blockquote className="mt-2 text-sm text-gray">{company}</blockquote>
       </figure>
     );
   };
@@ -83,16 +83,16 @@ const ReviewCard = ({
   export function MarqueeDemo() {
     return (
       <div className="relative flex h-[350px] w-full flex-col items-center justify-center overflow-hidden rounded-lg  bg-white md:shadow-xl">
-        <Marquee pauseOnHover className="[--duration:20s]">
-          {firstRow.map((review) => (
-            <ReviewCard key={review.username} {...review} />
+        {/* <Marquee pauseOnHover className="[--duration:20s]">
+          {firstRow.map((review: any) => (
+            <ReviewCard key={review.name} {...review} />
           ))}
         </Marquee>
         <Marquee reverse pauseOnHover className="[--duration:20s]">
-          {secondRow.map((review) => (
-            <ReviewCard key={review.username} {...review} />
+          {secondRow.map((review: any) => (
+            <ReviewCard key={review.name} {...review} />
           ))}
-        </Marquee>
+        </Marquee> */}
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-white"></div>
         <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-white"></div>
       </div>
