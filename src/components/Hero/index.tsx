37633c351a88beece236703b76c2d818e7142004
Shrey Anish G <shreyanish.g@gmail.com>
2024-08-05 01:34:00 +0530

    ilu inc again

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
 
   return (
     <div className="relative flex h-[700px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-[#111213]">
-      <Image src={ilu} alt="" className="mb-6 w-48 md:w-72"></Image>
+      <Image src={ilu} alt="" className="mb-6 w-64 md:w-[20rem]"></Image>
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b to-gray-950 from-white bg-clip-text text-center text-8xl font-bold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         DEMO DAY
       </span>
